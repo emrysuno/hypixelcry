45812855f1abd29baf9535fe2797a60aeac9b691
goon <goon@gmail.com>
2026-02-21 23:38:50 +0530

    a lot of additions

--- fish.lua
+++ fish.lua
@@ -2,10 +2,11 @@ local gui = require("goonUi.lua")
 local gog = require("goonLog.lua")
 local gut = require("goonUtils.lua")
 local rot = require("rotations_v2.lua")
+local gnl = require("notificationsLinux.lua")
 
 -- config ----------------------------------------------------------------------
 
--- don't modify this
+-- don't modify this, these are options for atkMethod (below this)
 local atkMethods = {
   none = "none", -- doesn't attack by any means, just keeps fishing
   oneshot = "oneshot", -- just uses the weapon once and goes back to fishing
@@ -13,7 +14,7 @@ local atkMethods = {
 }
 
 -- method to use for killing mobs, use the list above to choose one
-local atkMethod = atkMethods.oneshot
+local atkMethod = atkMethods.none
 -- value for oneshotEvery atkMethod, oneshots every x amounts you catch something
 local oneshotEveryValue = 2
 
@@ -23,25 +24,47 @@ local SLOTS = {
   EW = 6
 }
 
+local lookDirection = { yaw = -90, pitch = -0 }
+local alertWhenNotLookingAtDirection = true
+
 -- position yourself in a specific spot
 -- also the toggle for auto-buying rain
 local togglePositioning = true
+
+-- recast if nothing caught in x ticks value
+local recastIfNothingCaughtInXTicks = 80
 
 -- tick ranges (random cooldown before doing stuff)
 local CATCH = {2, 4} -- for catching after detection
 local ATTACK = {1, 2} -- for attacking after catching
 local RECAST = {2, 5} -- for recasting after catching
 
+rot.setRotationSpeed(20)
+
 --- config end -----------------------------------------------------------------
 
 local ew = {
   fishing = {
-    block = { -307.5, 71.5, -60.5 },
-    pos = { x = -307.5, y = 72, z = -60.5 }
+    block = { -314.5, 72, -59.5 },
+    pos = { x = -314.5, y = 72, z = -59.5 }
   },
   rain = {
     block = { -304.5, 75.5, -76.5 },
     pos = { x = -304.5, y = 76, z = -76.5 }
+  }
+}
+local scs = {
+  carrot_king = {
+    str = "§aIs this even a fish? It's the Carrot King!",
+    catches = 0
+  },
+  squid = {
+    str = "§aA Squid appeared.",
+    catches = 0
+  },
+  night_squid = {
+    str = "§aPitch darkness reveals a Night Squid.",
+    catches = 0
   }
 }
 local states = {
@@ -52,20 +75,42 @@ local states = {
   attacking = "attacking",
   recasting = "recasting",
   tpingToRain = "tpingToRain",
-  buyingRain = "buyingRain"
+  buyingRain = "buyingRain",
 }
 local state = states.idle
 local tick = 0
-local wait = 0
+local caughtFastest = 99
+local caughtSlowest = 0
+local caughtInSigma = 0
 local caught = 0
+local caughtLastUUID = nil
+local wait = 0
+local waitRotationAlert = 20
+local location = "idk"
 gog.config.logTypes.info.enabled = true
+gog.config.logTypes.debug.enabled = true
 gui.config.gapInLinesFromTop = 1
 gut.tgl.rain = true
 
 --- helper functions -----------------------------------------------------------
 
+local function isRainLow()
+  if togglePositioning
+    and gut.inf.rain < 10
+  then
+    return true
+  end
+  return false
+end
+
 local function tickProceed()
   tick = tick + 1
+end
+
+local function waitRotationAlertProceed()
+  if waitRotationAlert == 0 then return end
+  if waitRotationAlert < 0 then waitRotationAlert = 0 return end
+  waitRotationAlert = waitRotationAlert - 1
 end
 
 local function resetWait()
@@ -100,7 +145,7 @@ end
 registerClientTickPre(function()
 
   -- consistent stuff but before early exists
-  local location = player.getRawLocation()
+  location = player.getRawLocation()
   local curSlot = player.input.getSelectedSlot()
   local anyScreenOpened = player.inventory.isAnyScreenOpened()
   local title = player.inventory.getChestTitle()
@@ -117,6 +162,7 @@ registerClientTickPre(function()
 
   -- consistent stuff
   tickProceed()
+  waitRotationAlertProceed()
   local rod = player.fishHook
   local pos = player.getPos()
   local curRot = player.getRotation()
@@ -144,6 +190,15 @@ registerClientTickPre(function()
     and pos.y == ew.fishing.pos.y
     and pos.z == ew.fishing.pos.z
     then
+
+      -- look in fishing direction
+      if curRot.pitch ~= lookDirection.pitch
+      and curRot.yaw ~= lookDirection.yaw
+      then
+        rot.rotateToYawPitch(lookDirection.yaw, lookDirection.pitch)
+        return
+      end
+
       stateSwitch(states.recasting)
       return
     end
@@ -165,10 +220,10 @@ registerClientTickPre(function()
     end
 
     -- world.getRotation is buggy when yaw is 0, it just doesn't return the expected values, instead returns the current yaw/pitch ? tf idk
-    if curRot.yaw == 0 then
-      rot.rotateToYawPitch(curRot.yaw + 0.1, curRot.pitch)
-      return
-    end
+    -- if curRot.yaw == 0 then
+    --   rot.rotateToYawPitch(curRot.yaw + 0.1, curRot.pitch)
+    --   return
+    -- end
 
     local worldRot = world.getRotation(ew.fishing.block[1], ew.fishing.block[2], ew.fishing.block[3])
     if curRot.yaw ~= worldRot.yaw
@@ -179,7 +234,7 @@ registerClientTickPre(function()
     end
 
     player.input.silentUse(SLOTS.EW)
-    gog.info("etherwarped to fishing pos")
+    gog.debug("etherwarped to fishing pos")
     player.input.setPressedSneak(false)
     wait = 20
     resetTick()
@@ -190,11 +245,24 @@ registerClientTickPre(function()
     if tick < wait then return end
     resetWait()
 
-    -- idle
+    -- going idle cuz rod not out for 3.5s, while fishing
     if rod == nil
-    and tick > 40
+    and tick > 70
     then
       stateSwitch(states.idle)
+      gog.debug("going idle cuz rod not out for 3.5s, while fishing")
+      gnl.snowNotify("skyblock", "going idle")
+    end
+
+    -- alert about direction
+    if alertWhenNotLookingAtDirection
+    and curRot.yaw ~= lookDirection.yaw
+    and curRot.pitch ~= lookDirection.pitch
+    and waitRotationAlert == 0
+    then
+      gog.critical("rotation alert")
+      gnl.snowNotifty("skyblock", "rotation alert")
+      waitRotationAlert = 40
     end
 
     -- position
@@ -203,15 +271,22 @@ registerClientTickPre(function()
     or pos.y ~= ew.fishing.pos.y
     or pos.z ~= ew.fishing.pos.z)
     then
+      -- if rabbit nearby then silent use atk then wait 5t
       stateSwitch(states.tpingToFishing)
       return
     end
 
     -- low rain detection
-    if togglePositioning
-    and gut.inf.rain < 10
-    then
+    if isRainLow() then
       stateSwitch(states.tpingToRain)
+      return
+    end
+
+    -- recast if taking too long
+    if tick > recastIfNothingCaughtInXTicks then
+      gog.debug("recasting cuz nothing caught for a while")
+      player.input.silentUse(SLOTS.ROD)
+      stateSwitch(states.recasting)
       return
     end
 
@@ -219,11 +294,18 @@ registerClientTickPre(function()
     local entities = world.getEntities()
     for _, entity in ipairs(entities) do
       local name = entity.name
+      local uuid = entity.uuid
       if name
       and (string.find(name, "!!!")
       or string.find(name, "ǃǃǃ")
       or string.find(name, "ꜝꜝꜝ"))
+      and uuid ~= caughtLastUUID
       then
+        caughtLastUUID = uuid
+        gog.debug("detected in " .. tostring(tick))
+        caughtInSigma = caughtInSigma + tick
+        if tick < caughtFastest then caughtFastest = tick end
+        if tick > caughtSlowest then caughtSlowest = tick end
         stateSwitch(states.catching)
         break
       end
@@ -236,7 +318,7 @@ registerClientTickPre(function()
     resetWait()
     caught = caught + 1
     player.input.silentUse(SLOTS.ROD)
-    gog.info("caught")
+    gog.debug("caught")
 
     -- decide next state weather to atk or not
     local new_state = states.recasting
@@ -266,7 +348,7 @@ registerClientTickPre(function()
 
     -- attack
     player.input.silentUse(SLOTS.ATK)
-    gog.info("attacked")
+    gog.debug("attacked")
     stateSwitch(states.recasting)
 
   -- recasting -----------------------------------------------------------------
@@ -278,21 +360,14 @@ registerClientTickPre(function()
     -- re-equip rod
     if curSlot and curSlot ~= SLOTS.ROD then
       player.input.setSelectedSlot(SLOTS.ROD)
-      gog.info("selected rod slot")
+      gog.debug("selected rod slot")
       wait = 2 -- delay after equipping rod
       resetTick()
       return
     end
 
-    -- look down
-    if curRot.pitch ~= 90
-    then
-      rot.rotateToYawPitch(curRot.yaw, 90)
-      return
-    end
-
     player.input.silentUse(SLOTS.ROD)
-    gog.info("recasted")
+    gog.debug("recasted")
     stateSwitch(states.fishing)
 
   -- tpingToRain ---------------------------------------------------------------
@@ -341,11 +416,12 @@ registerClientTickPre(function()
     end
 
     player.input.silentUse(SLOTS.EW)
-    gog.info("etherwarped to rain pos")
+    gog.debug("etherwarped to rain pos")
     player.input.setPressedSneak(false)
     wait = 20
     resetTick()
 
+  -- buyingRain ----------------------------------------------------------------
   elseif state == states.buyingRain then
 
     if tick < wait then return end
@@ -353,8 +429,15 @@ registerClientTickPre(function()
 
     -- stop when 28m of rain bought (rain number never goes above 1740)
     if gut.inf.rain > 1680 then
-      player.inventory.closeScreen()
+      gog.debug("enough rain bought")
+      if anyScreenOpened then
+        player.inventory.closeScreen()
+        wait = 11
+        resetTick()
+        return
+      end
       stateSwitch(states.tpingToFishing)
+      return
     end
 
     -- position
@@ -368,7 +451,7 @@ registerClientTickPre(function()
 
     -- look at npc
     if curRot.yaw ~= -179.0 then
-      rot.rotateToYawPitch(-179, curRot.pitch)
+      rot.rotateToYawPitch(-179.0, 20)
       return
     end
 
@@ -387,18 +470,78 @@ registerClientTickPre(function()
 
 end)
 
+local graySlash = gut.clr.gray .. "/"
+
 register2DRenderer(function(context)
 
-  local pos = player.getPos()
+  -- local pos = player.getPos()
 
-  gui.content = {
+  local scsCaught = 0
+  local content = {
+
     { text = "state: " .. state },
     { text = "tick: " .. tick },
     { text = "wait: " .. wait },
+    { text = "rain: " .. gut.inf.rain },
     { text = "caught: " .. caught },
-    { text = "rot: " .. tostring(rot.isRotating()) },
-    { text = "rain: " .. gut.inf.rain }
+    { text = "caught times: " ..
+      gut.clr.green .. tostring(caughtFastest) ..
+      graySlash ..
+      gut.clr.yellow .. tostring(gut.roundUpToTwoDecimals(caughtInSigma / caught)) ..
+      graySlash ..
+      gut.clr.red .. tostring(caughtSlowest) }
+
   }
+
+  for key, data in pairs(scs) do
+    table.insert(
+      content, {
+        text =
+          key .. ": " ..
+          tostring(data.catches) ..
+          graySlash ..
+          gut.clr.yellow ..
+          tostring(gut.roundUpToTwoDecimals((data.catches / caught) * 100))
+      }
+    )
+    scsCaught = scsCaught + data.catches
+  end
+  local uselessCatches = caught - scsCaught
+  table.insert(content, {
+    text = "useless: " ..
+      uselessCatches ..
+      graySlash ..
+      gut.clr.yellow ..
+      tostring(gut.roundUpToTwoDecimals((uselessCatches / caught) * 100))
+  })
+
+  table.insert(content, { text = "" })
+  table.insert(content, { text = "[debug]" })
+  table.insert(content, { text = "rotating: " .. tostring(rot.isRotating()) })
+  table.insert(content, { text = "waitRotationAlert: " .. waitRotationAlert })
+  table.insert(content, { text = "location: " .. location })
+
+  gui.content = content
+
+end)
+
+registerMessageEvent(function(text, overlay, json)
+
+  if overlay then return end
+  if json then return end
+  if not text then return end
+  -- print("                  =======>>>>>> " .. text)
+
+  if text == scs.carrot_king.str then
+    scs.carrot_king.catches = scs.carrot_king.catches + 1
+
+  elseif text == scs.squid.str then
+    scs.squid.catches = scs.squid.catches + 1
+
+  elseif text == scs.night_squid.str then
+    scs.night_squid.catches = scs.night_squid.catches + 1
+
+  end
 
 end)
 
