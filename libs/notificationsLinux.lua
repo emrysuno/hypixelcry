c12f91e80e9f4f75d8771bddd533325d44e8ca0d
goon <goon@gmail.com>
2026-02-22 00:14:59 +0530

    param for urgency & timeout, default values for same

--- libs/notificationsLinux.lua
+++ libs/notificationsLinux.lua
@@ -25,13 +25,41 @@ local function simpleEncode(str)
   return result
 end
 
-function notifications.snowNotifty(title, message)
+-- for infinite duration use 0 (notification doesn't disappear)
+notifications.defaultTimeout = 5000
+-- low / normal / critical
+notifications.defaultUrgency = "normal"
+
+function notifications.snowNotifty(title, message, urgency, timeout)
   -- Используем encodeURIComponent через JavaScript если доступен
   -- или простую функцию кодирования
   local encodedTitle = simpleEncode(title)
   local encodedMessage = simpleEncode(message)
 
-  local url = "http://localhost:8080/notify?title=" .. encodedTitle .. "&message=" .. encodedMessage
+  local url =
+  "http://localhost:8080/notify?title=" .. encodedTitle ..
+  "&message=" .. encodedMessage
+
+  -- urgency
+  local encodedUrgency
+  if urgency
+  and (urgency == "low" or urgency == "normal" or urgency == "critical")
+  then
+    encodedUrgency = simpleEncode(urgency)
+  else
+    encodedUrgency = simpleEncode(notifications.defaultUrgency)
+  end
+  url = url .. "&urgency=" .. encodedUrgency
+
+  -- timeout
+  local encodedTimeout
+  if timeout
+  and type(timeout) == "number" then
+    encodedTimeout = simpleEncode(timeout)
+  else
+    encodedTimeout = simpleEncode(notifications.defaultTimeout)
+  end
+  url = url .. "&timeout=" .. encodedTimeout
 
   http.get_async_callback(url, function(response, error)
 
